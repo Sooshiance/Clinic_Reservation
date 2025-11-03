97df32e9a5d070b1e02b3e08b88a4944e5bc898f
sooshiance <s.katafract@gmail.com>
2025-11-03 22:01:06 +0330

    feat: Add scanner to find nearbu WiFis

---
+++ scanner.py
@@ -0,0 +1,27 @@
+import subprocess
+
+
+def scan_wifi_linux():
+    try:
+        result = subprocess.check_output(
+            [
+                "nmcli",
+                "-t",
+                "-f",
+                "SSID,signal",
+                "device",
+                "wifi",
+            ],
+            encoding="utf-8",
+        )
+        networks = []
+        for line in result.strip().split("\n"):
+            ssid, signal = line.split(":")
+            networks.append({"ssid": ssid, "signal": signal})
+        return networks
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+
+print(scan_wifi_linux())
